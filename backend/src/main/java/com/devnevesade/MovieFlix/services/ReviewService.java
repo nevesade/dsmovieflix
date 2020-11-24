4292ae89bd2fbf2a5a9b8eb8b4c8b408e775330e
nevesade <adenasiocarvalho19@gmail.com>
2020-11-24 19:57:23 +0000

    CRUD Review

--- backend/src/main/java/com/devnevesade/MovieFlix/services/ReviewService.java
+++ backend/src/main/java/com/devnevesade/MovieFlix/services/ReviewService.java
@@ -1,16 +1,22 @@
 package com.devnevesade.MovieFlix.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 import com.devnevesade.MovieFlix.dto.ReviewDTO;
 import com.devnevesade.MovieFlix.entities.Review;
 import com.devnevesade.MovieFlix.repositories.ReviewRepository;
+import com.devnevesade.MovieFlix.services.exceptions.DatabaseException;
+import com.devnevesade.MovieFlix.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ReviewService {
@@ -19,10 +25,62 @@ public class ReviewService {
 	private ReviewRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ReviewDTO> findAll(){
-		List<Review> list = repository.findAll();
+	public Page<ReviewDTO> findAllPaged(PageRequest pageRequest){
+		Page<Review> list = repository.findAll(pageRequest);
 		
-		return  list.stream().map( x -> new ReviewDTO(x)).collect(Collectors.toList());
+		return  list.map( x -> new ReviewDTO(x));
 	}
+	
+	@Transactional(readOnly = true )
+	public ReviewDTO findById(Long id) {
+		Optional<Review> obj = repository.findById(id);
+		Review entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ReviewDTO(entity);
+		
+	}
+	
+	
+	@Transactional
+	public ReviewDTO insert(ReviewDTO dto) {
+		Review  entity = new Review();
+		entity.setText(dto.getText());
+		
+		entity = repository.save(entity);
+		return new ReviewDTO(entity);
+	}
+	
+	
+
+	@Transactional
+	public ReviewDTO updtate(Long id, ReviewDTO dto) {
+		try {
+			Review entity = repository.getOne(id);
+			entity.setText(dto.getText());
+			entity = repository.save(entity);
+			return new ReviewDTO(entity);
+		
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	
+	}
+
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new  ResourceNotFoundException("Id mot Found "  + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+	}
+	
+
+	
 
 }
