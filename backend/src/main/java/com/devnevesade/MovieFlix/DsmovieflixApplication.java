274912d721da060e652a8b4485d9b866ad8a6699
nevesade <adenasiocarvalho19@gmail.com>
2020-11-24 09:40:57 +0000

    Project MovieFlix created

---
+++ backend/src/main/java/com/devnevesade/MovieFlix/DsmovieflixApplication.java
@@ -0,0 +1,13 @@
+package com.devnevesade.MovieFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieflixApplication.class, args);
+	}
+
+}
