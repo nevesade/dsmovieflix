33e4be5d261705b54d165ad92d3ea68f2d758471
nevesade <adenasiocarvalho19@gmail.com>
2020-11-26 14:43:09 +0000

    Token aditional information like name and userId

--- backend/src/main/java/com/devnevesade/MovieFlix/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devnevesade/MovieFlix/config/AuthorizationServerConfig.java
@@ -2,6 +2,8 @@ package com.devnevesade.MovieFlix.config;
 
 
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +14,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import com.devnevesade.MovieFlix.components.JwtTokenEnhancer;
 
 
 @Configuration
@@ -46,11 +51,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	private AuthenticationManager authenticationManager;
 	
 	
-	/*
+	
 	@Autowired
 	private JwtTokenEnhancer tokenEnhancer;
 	
-	*/
+	
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -71,19 +76,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		
-		/*
+		
 		TokenEnhancerChain chain = new TokenEnhancerChain();
-		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter ));
-		*/
+		chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer ));
+		
 		
 		endpoints.authenticationManager(authenticationManager)
 		.tokenStore(tokenStore)
-		.accessTokenConverter(accessTokenConverter);
-		
-		/*
+		.accessTokenConverter(accessTokenConverter)
 		.tokenEnhancer(chain);
 		
-		*/
+		
 	}
 	
 	
